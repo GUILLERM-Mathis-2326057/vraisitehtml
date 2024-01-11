8a18e8f546474898ae695cc67233204e206c76fa
mathis guillerm <mathis.guillerm@etu.univ-amu.fr>
2024-01-11 11:11:36 +0100

    tj

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -4,9 +4,9 @@ checkbox.addEventListener("change",(elem) => {
   console.log(elem.checked);
   let baliseStyle = document.querySelector("#colorCssFile");
 
-  if (balise.style.getAttribute("href") === "../css/light.css"){
-    baliseStyle;setAttribute("href","../css/dark.css");
+  if (balise.style.getAttribute("href") === "light.css"){
+    baliseStyle;setAttribute("href","dark.css");
     return
   }
-  baliseStyle.setAttribute("href","../css/light.css");
+  baliseStyle.setAttribute("href","light.css");
 });
